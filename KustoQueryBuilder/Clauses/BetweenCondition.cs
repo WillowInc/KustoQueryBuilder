681823aaf689848dd9e8050eade5873c94f405b0
JO Reyes <joreyes@willowinc.com>
2022-08-26 14:05:03 +1000

    where between functionality

---
+++ KustoQueryBuilder/Clauses/BetweenCondition.cs
@@ -0,0 +1,28 @@
+﻿namespace KustoQueryBuilder.Clauses;
+
+public class BetweenCondition : AbstractClause
+{
+    public string Column { get; }
+    public string FromValue { get; }
+    public string ToValue { get; }
+
+    public BetweenCondition(string column, string fromValue, string toValue)
+    {
+        if (string.IsNullOrEmpty(column))
+        {
+            throw new ArgumentNullException(nameof(column));
+        }
+
+        if (string.IsNullOrEmpty(fromValue))
+        {
+            throw new ArgumentNullException(nameof(fromValue));
+        }
+
+        ArgumentNullException.ThrowIfNull(fromValue);
+        ArgumentNullException.ThrowIfNull(toValue);
+
+        Column = column;
+        FromValue = fromValue;
+        ToValue = toValue;
+    }
+}
--- KustoQueryBuilder/Query.Where.cs
+++ KustoQueryBuilder/Query.Where.cs
@@ -16,4 +16,11 @@ public partial class Query
 
         return this;
     }
+    
+    public Query WhereBetween(string column, string fromValue, string toValue)
+    {
+        _clauses.Add(new BetweenCondition(column, fromValue, toValue));
+
+        return this;
+    }
 }
