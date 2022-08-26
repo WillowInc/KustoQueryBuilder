526c8c5dae053de19d80a4c31a57db5134626f58
Rochit Sen <RSen@willowinc.com>
2022-08-26 14:01:19 +0930

    resolving comments.

--- KustoQueryBuilder/Clauses/InClause.cs
+++ KustoQueryBuilder/Clauses/InClause.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KustoQueryBuilder.Clauses
+﻿namespace KustoQueryBuilder.Clauses
 {
-    
+
     public class InClause : AbstractClause
     {
         public string Column { get; }
-        public List<string> Values { get; set; }
+        public List<string> Values { get; }
         public InClause(string column, List<string> values)
         {
             if (string.IsNullOrEmpty(column))
