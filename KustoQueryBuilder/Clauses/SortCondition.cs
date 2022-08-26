8a77ac72c96a08d078943fdf6c2747ae817e3dbf
Ramhari Gyawali <rgyawali@willowinc.com>
2022-08-26 14:16:06 +1000

    Add support for OrderBy clause

---
+++ KustoQueryBuilder/Clauses/SortCondition.cs
@@ -0,0 +1,13 @@
+﻿namespace KustoQueryBuilder.Clauses;
+
+internal class SortCondition : AbstractClause
+{
+    public SortCondition(bool isDescending, string[] sortColums)
+    {
+        IsDescending = isDescending;
+        SortColums = sortColums;
+    }
+
+    public bool IsDescending { get; }
+    public string[] SortColums { get; }
+}
