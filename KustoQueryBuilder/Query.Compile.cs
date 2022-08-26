9d801aefa8e79650c23e5ae26490b09ffaac6705
Ramhari Gyawali <rgyawali@willowinc.com>
2022-08-26 16:13:52 +1000

    Fix failing test

--- KustoQueryBuilder/Query.Compile.cs
+++ KustoQueryBuilder/Query.Compile.cs
@@ -55,7 +55,7 @@ public partial class Query
     //todo enable specifying sort order for each column
     private void AppendSortClause(SortClause sortClause)
     {
-        Append($"order by {string.Join(", ", sortClause.SortColums.Select(s => s + (sortClause.IsDescending ? " desc" : " asc")))}");
+        Append($"order by {string.Join(", ", sortClause.SortColums.Select(s => s + (sortClause.IsDescending ? " desc" : string.Empty)))}");
     }
 
     private void AppendBetweenClause(BetweenClause betweenClause)
