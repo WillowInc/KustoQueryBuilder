cd83bc68f1f4550e9c4b1afcba866e5bfe127b9e
Rochit Sen <RSen@willowinc.com>
2022-08-26 15:46:45 +0930

    Where-in-test.

--- KustoQueryBuilder.UnitTests/QueryTests.cs
+++ KustoQueryBuilder.UnitTests/QueryTests.cs
@@ -10,4 +10,14 @@ public class QueryTests
     {
         Assert.Throws<ArgumentException>(() => new Query(tableName));
     }
+
+    [Fact]
+    public void Query_Where_In_ShouldBeValid()
+    {
+        var q = new Query("Table1")
+            .WhereIn("Col1", new[] { "A", "B", "C" })
+            .Compile();
+
+        
+    }
 }
