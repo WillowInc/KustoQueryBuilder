8137100024d919cff177035cb8cec10e5255494d
JO Reyes <joreyes@willowinc.com>
2022-08-26 15:52:48 +1000

    unit test fixes

--- KustoQueryBuilder.UnitTests/QueryWhereTests.cs
+++ KustoQueryBuilder.UnitTests/QueryWhereTests.cs
@@ -6,7 +6,7 @@ public class QueryWhereTests
     public void Query_WhereBetweenValidDates_ReturnsQuery()
     {
         var q = new Query("Table1")
-            .WhereBetween("Column1", new DateTime(2021, 01, 01), new DateTime(2022, 01, 01));
+            .WhereBetween("Column1", new DateTime(2021, 1, 1), new DateTime(2022, 1, 1));
 
         var actualQuery = q.Compile();
         var expectedQuery = "Table1\n | where Column1 between (datetime(1/01/2022 12:00:00 AM) .. datetime(1/01/2021 12:00:00 AM))";
@@ -18,7 +18,7 @@ public class QueryWhereTests
     public void Query_WhereNotBetweenValidDates_ReturnsQuery()
     {
         var q = new Query("Table1")
-            .WhereNotBetween("Column1", new DateTime(2021, 01, 01), new DateTime(2022, 01, 01));
+            .WhereNotBetween("Column1", new DateTime(2021, 1, 1), new DateTime(2022, 1, 1));
 
         var actualQuery = q.Compile();
         var expectedQuery = "Table1\n | where Column1 !between (datetime(1/01/2022 12:00:00 AM) .. datetime(1/01/2021 12:00:00 AM))";
