ba8ddc336ceb86f7033b4a7b85e885478ee55ddf
Rochit Sen <RSen@willowinc.com>
2022-08-26 16:20:02 +0930

    Added test for where-in clause.

--- KustoQueryBuilder.UnitTests/QueryTests.cs
+++ KustoQueryBuilder.UnitTests/QueryTests.cs
@@ -14,10 +14,13 @@ public class QueryTests
     [Fact]
     public void Query_Where_In_ShouldBeValid()
     {
-        var q = new Query("Table1")
+        var actualQuery = new Query("Table1")
             .WhereIn("Col1", new[] { "A", "B", "C" })
             .Compile();
 
+        var expectedQuery = "Table1\n" + @" | where Col1 in (""A"", ""B"", ""C"")";
+
+        Assert.Equal(expectedQuery, actualQuery);
         
     }
 }
